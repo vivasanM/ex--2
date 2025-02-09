e6988d86561c19697027a8ebe43a5419fed65291
vivasanM <vivasan2006@gmail.com>
2025-02-09 10:16:28 +0530

    Add files via upload

---
+++ q12/odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter the value:");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("IT IS AN EVEN NUMBER");
+}
+else
+{
+printf("IT IS AN ODD NUMBER");
+}
+return 0;
+}
